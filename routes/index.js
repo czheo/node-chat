317f96f71cc177bd2b09b9243d1a3292e8b1eea9
czheo <czheo1987@gmail.com>
2014-05-25 17:39:05 +0000

    add ip banning: redis.sadd('banned_ip', '0.0.0.0')

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,19 @@ var redis = require("../redis");
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index');
+    // is banned ip?
+    var ip = req.connection.remoteAddress;
+    redis.sismember("banned_ip", ip, function(err, result){
+        if(err) {
+            console.log(err);
+            return false;
+        }
+        if(result == 1) {
+            res.send("you are banned!");
+        } else {
+            res.render('index');
+        }
+    });
 });
 
 module.exports = router;
