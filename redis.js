0b3f226f8a1f46a7c094e5df663275bba33b2f75
czheo <czheo1987@gmail.com>
2015-06-02 18:45:30 +0900

    fix ENV

--- redis.js
+++ redis.js
@@ -1,2 +1,8 @@
-var redis = require("redis").createClient(6379, "redis");
+var redis = require("redis").createClient(
+    process.env.REDIS_PORT_6379_TCP_PORT || 6379,
+    process.env.REDIS_PORT_6379_TCP_ADDR || "redis",
+    {
+        auth_pass: process.env.REDIS_PASSWORD || null
+    }
+);
 module.exports = redis;
